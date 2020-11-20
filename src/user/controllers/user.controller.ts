69c4a9a9e1bd6879e8f3df61960022aced148675
Hugo Virgen <virgenherrera@gmail.com>
2020-11-20 00:07:07 -0600

    add swagger missing config

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -7,10 +7,12 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto, UpdateUserDto } from '../dto';
 import { UserService } from '../services';
 import { userRoutes } from '../user.routes';
 
+@ApiTags('Users')
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
--- src/utils/swagger-setup.util.ts
+++ src/utils/swagger-setup.util.ts
@@ -4,23 +4,25 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 export function swaggerSetup(app: INestApplication) {
-    const swaggerRoutePath = 'api-docs';
-    const afterListenLog = {
-        swaggerSetup: `Swagger docs available on path: :url/${swaggerRoutePath}`,
-    };
-    const fileContent = readFileSync(join(__dirname, '../../package.json'), {
-        encoding: 'utf8',
-        flag: 'r',
-    });
-    const packageJson = JSON.parse(fileContent);
-    const swaggerConfig = new DocumentBuilder()
-        .setTitle(packageJson.name)
-        .setVersion(packageJson.version)
-        .setDescription(packageJson.description)
-        .build();
+  const swaggerRoutePath = 'api-docs';
+  const afterListenLog = {
+    swaggerSetup: `Swagger docs available on path: :url/${swaggerRoutePath}`,
+  };
+  const fileContent = readFileSync(join(__dirname, '../../package.json'), {
+    encoding: 'utf8',
+    flag: 'r',
+  });
+  const packageJson = JSON.parse(fileContent);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(packageJson.name)
+    .setVersion(packageJson.version)
+    .setDescription(packageJson.description)
+    .build();
 
-    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup(swaggerRoutePath, app, swaggerDocument);
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(swaggerRoutePath, app, swaggerDocument, {
+    explorer: true,
+  });
 
-    return afterListenLog;
+  return afterListenLog;
 }
