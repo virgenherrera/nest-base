7907dedeacae6f64343a59dc9c37e75bfd50ae0e
Hugo Virgen <virgenherrera@gmailcom>
2021-01-12 16:32:43 -0600

    - install joi - add validationSchema to rootConfigModule

--- src/core/config/config-module.options.ts
+++ src/core/config/config-module.options.ts
@@ -1,4 +1,5 @@
 import { ConfigModuleOptions } from '@nestjs/config/dist/interfaces';
+import * as Joi from 'joi';
 import { join } from 'path';
 import { appConfig } from './app.config';
 import { typeOrmConfig } from './typeorm.config';
@@ -7,4 +8,16 @@ export const rootConfigModule: ConfigModuleOptions = {
   isGlobal: true,
   load: [appConfig, typeOrmConfig],
   envFilePath: join(__dirname, '../../../.env'),
+  validationSchema: Joi.object({
+    // # APP
+    APP_PORT: Joi.number().port().required(),
+    APP_USE_DOCS: Joi.boolean().default(false).required(),
+
+    // # Database connection
+    DATABASE_HOST: Joi.string().required(),
+    DATABASE_PORT: Joi.number().port().required(),
+    DATABASE_USERNAME: Joi.string().required(),
+    DATABASE_PASSWORD: Joi.string().required(),
+    DATABASE_NAME: Joi.string().required(),
+  }),
 };
