3742f3cd2a8a9ea3614c88e82641511da0f6dee0
Hugo Virgen <virgenherrera@gmail.com>
2024-06-20 11:55:21 -0600

    task: Bump deps
    - run bumpDependencies script.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { CommonModule } from './common/common.module';
-import {
-  GlobalValidationPipeProvider,
-  LogRequestInterceptorProvider,
-} from './common/providers';
+import * as AppProviders from './common/providers';
 import { AppConfigModule } from './imports';
 
 @Module({
   imports: [AppConfigModule.forRoot(), CommonModule],
-  providers: [LogRequestInterceptorProvider, GlobalValidationPipeProvider],
+  providers: Object.values(AppProviders),
 })
 export class AppModule {}
--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,5 @@
 export default async function Setup() {
   // set E2E TEST ENV Variables
-  process.env.NODE_ENV = 'TEST';
+  process.env.NODE_ENV = 'E2E';
   process.env.APP_PORT = '0';
 }
