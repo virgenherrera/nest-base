9e7cb6543f3e930c498d2f157060bfe4bfe060f5
Hugo Virgen <virgenherrera@gmail.com>
2024-06-21 12:11:27 -0600

    task: Import Node Process
    - delete process. expressions in favor of import them.

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,9 @@
+import { env } from 'node:process';
+
 export default async function Setup() {
   // set E2E TEST ENV Variables
-  process.env.NODE_ENV = 'E2E';
-  process.env.APP_PORT = '0';
+  Object.assign(env, {
+    NODE_ENV: 'E2E',
+    APP_PORT: '0',
+  });
 }
