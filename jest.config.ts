1ab4bf5c6953a4a3059d6f81056e0b37320220e5
Hugo Virgen <virgenherrera@gmail.com>
2026-05-29 11:16:36 -0600

    refactor: unify Jest configs and consolidate test infrastructure
    Merge jest.config.ts and jest-e2e.config.ts into a single config with
    rootDir='.', eliminating duplicate Jest bootstrap overhead. Rename
    test:unit/test:e2e to test:dynamic for consistency with test:static.

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,5 @@
 /** @jest-config-loader ts-node */
+/** @jest-config-loader-options {"transpileOnly": true} */
 
 /*
  * For a detailed explanation regarding each configuration property and type check visit:
@@ -10,23 +11,22 @@ export class JestConfig implements Config {
   cache = false;
   collectCoverage = true;
   collectCoverageFrom = [
-    '<rootDir>/**/*.(controller|filter|guard|middleware|pipe|service|util).ts',
-    '<rootDir>/!**/*.spec.ts',
-    '<rootDir>/!**/(index|main).ts',
-    '<rootDir>/!**/test/**',
+    '<rootDir>/src/**/*.(controller|filter|guard|middleware|pipe|service|util).ts',
+    '!<rootDir>/src/**/*.spec.ts',
+    '!<rootDir>/src/**/(index|main).ts',
   ];
-  coverageDirectory = '../coverage/unit';
+  coverageDirectory = '<rootDir>/coverage';
   coverageReporters: Config['coverageReporters'] = ['html-spa'];
   coverageThreshold = {
     global: { branches: 80, functions: 80, lines: 80, statements: 80 },
   };
   detectOpenHandles = true;
-  globalSetup = `<rootDir>/../test/utils/unit.setup.ts`;
+  globalSetup = '<rootDir>/test/utils/test.setup.ts';
   moduleFileExtensions = ['js', 'json', 'ts'];
   reporters = ['default', 'summary', 'github-actions'];
-  rootDir = './src';
+  rootDir = '.';
   testEnvironment = 'node';
-  testRegex = '.*\\.spec\\.ts$';
+  testRegex = '(src|test)/.*\\.(spec|e2e-spec)\\.ts$';
   testTimeout = 3e3;
   transform = { '^.+\\.ts$': 'ts-jest' };
   verbose = true;
--- test/utils/test.setup.ts
+++ test/utils/test.setup.ts
@@ -2,10 +2,9 @@ import { env } from 'node:process';
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export default async function Setup() {
-  // set E2E TEST ENV Variables
   Object.assign(env, {
     SERVER_PORT: '0',
-    APP_ENV: 'test:e2e',
+    APP_ENV: 'test',
     SWAGGER_ENABLED: 'false',
   });
 }
